2c06cadea1a631b1f8015a26cff29cd805456c6e
Crosimo <jean.deborsu@hotmail.com>
2021-06-29 10:57:05 +0200

    finishtouch

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+    state = {
+        data: new Date(),
+        
+    }
+    timed = {
+        timer: null
+    }
+    componentDidMount() {
+         this.timed.timer = setInterval(() => {
+          this.setState({data: new Date()})
+      },1000)
+    }
+    componentDidUnMount() {
+        clearInterval(this.timed.timer)
+    }
+    render() {
+        return (
+            <div>
+              Il est {this.state.data.getHours()}:{this.state.data.getMinutes()}:{this.state.data.getSeconds()} 
+            </div>
+        )
+    }
 }
 
-export default App;
